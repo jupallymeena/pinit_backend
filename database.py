4237635c40214db9b03f2c484f1d05629711edea
jupallymeena <meenajupally02@gmail.com>
2026-03-05 16:00:01 +0530

    Initial commit

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_url=os.getenv("database_url")
+db_url = os.getenv("DATABASE_URL")
 
 engine=create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
